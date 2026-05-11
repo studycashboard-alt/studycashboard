eec36bdd858933813edec74c5a474c371e2d6ad4
studycashboard-alt <studycashboard@gmail.com>
2026-05-10 19:13:36 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://astgazboqpwhcuyemshx.supabase.co";
 
-const supabaseAnonKey = "PASTE_YOUR_FULL_ANON_PUBLIC_KEY_HERE";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdGdhemJvcXB3aGN1eWVtc2h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNzUyMDAsImV4cCI6MjA5Mzg1MTIwMH0.9ZKI3MaZbJSWSm2QC2jOETrhGTiUETcvmZW4PCJWyk8";
 
 export const supabase = createClient(
   supabaseUrl,
